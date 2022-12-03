22eca40db25317fd250ca945f63790a500ea242c
HafizMuhammadRayyan <hafizmuhammmadrayyan@gmail.com>
2022-12-03 17:02:23 +0500

    first web with server

--- server.mjs
+++ server.mjs
@@ -1,10 +1,17 @@
 import express from 'express';
+import path from 'path';
+
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+app.get('/abc', (req, res) => {
+    console.log("Request ip ", req.ip);
     res.send('HELLO WORLD EXPRESS JS >>> ' + new Date().toString())
 })
+
+const __dirname = path.resolve();
+app.use('/', express.static(path.join(__dirname, './web/dist')))
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
---
+++ web/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World first server app</h1>
+    </div>
+  );
+}
+
+export default App;
